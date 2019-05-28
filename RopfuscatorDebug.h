8fdc8875d0985b79ed25e6fe698da9f1e33171bf
Giulio De Pasquale <me@giugl.io>
2019-05-28 15:08:22 +0100

    C++14 - MachineInstr custom formatter for fmtlib

--- RopfuscatorDebug.h
+++ RopfuscatorDebug.h
@@ -1,6 +1,7 @@
 #include "llvm/CodeGen/MachineInstr.h"
 #include "llvm/Support/Debug.h"
 #include "llvm/Support/raw_ostream.h"
+#include <fmt/format.h>
 
 #define XCHG_CHAIN "xchg_chains"
 #define ROPCHAIN "ropchains"
@@ -25,3 +26,18 @@
 #define COLOR_YELLOW "\x1b[33m"
 #define COLOR_BLUE "\x1b[34m"
 #define COLOR_RESET "\x1b[0m"
+
+template <> struct fmt::formatter<llvm::MachineInstr> {
+  template <typename ParseContext> constexpr auto parse(ParseContext &ctx) {
+    return ctx.begin();
+  }
+
+  template <typename FormatContext>
+  auto format(const llvm::MachineInstr &MI, FormatContext &ctx) {
+    std::string s;
+    llvm::raw_string_ostream rso(s);
+    MI.print(rso);
+
+    return fmt::format_to(ctx.begin(), "{}", rso.str());
+  }
+};
