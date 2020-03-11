2ebfca4af71fb62c4312cabc467117def23f7d2e
Giulio De Pasquale <giulio@depasquale.eu>
2020-03-11 20:57:49 +0000

    parsing and applying defaults (excluding algorithms)

--- Ropfuscator/ROPfuscatorConfig.h
+++ Ropfuscator/ROPfuscatorConfig.h
@@ -29,6 +29,7 @@ extern const std::string OPAQUE_BRANCH_ALGORITHM_ADDREG_MOV;
 #define CONFIG_OPA_PRED_ALGO "opaque_predicates_algorithm"
 #define CONFIG_BRANCH_DIV_ENABLED "branch_divergence_enabled"
 #define CONFIG_BRANCH_DIV_MAX "branch_divergence_max_branches"
+#define CONFIG_BRANCH_DIV_ALGO "branch_divergence_algorithm"
 
 //===========================
 
@@ -100,31 +101,115 @@ struct ROPfuscatorConfig {
       exit(-1);
     }
 
-    // checking if [general] is in the configuration
-    if (!configuration_data.contains(CONFIG_GENERAL_SECTION)) {
-      fmt::print("Could not find \"[{}]\" section in configuration file.\n",
-                 CONFIG_GENERAL_SECTION);
-      exit(-1);
+    // setting default values
+    globalConfig = GlobalConfig();
+    defaultParameter = ObfuscationParameter();
+
+    /* =====================================
+     * applying [general] section, if present
+     */
+    if (configuration_data.contains(CONFIG_GENERAL_SECTION)) {
+      toml::value general_section =
+          toml::find(configuration_data, CONFIG_GENERAL_SECTION);
+
+      // Custom library path
+      if (general_section.contains(CONFIG_CUSTOM_LIB_PATH)) {
+        auto library_path =
+            general_section.at(CONFIG_CUSTOM_LIB_PATH).as_string();
+
+        dbg_fmt("Setting library path to {}\n", library_path);
+        globalConfig.libraryPath = library_path;
+      }
+
+      // Avoid multiversion symbols
+      if (general_section.contains(CONFIG_AVOID_MULTIVER)) {
+        auto avoid_multiver =
+            general_section.at(CONFIG_AVOID_MULTIVER).as_boolean();
+
+        dbg_fmt("Setting {} flag to {}\n", CONFIG_AVOID_MULTIVER,
+                avoid_multiver);
+        globalConfig.avoidMultiversionSymbol = avoid_multiver;
+      }
+
+      // Search in segment
+      if (general_section.contains(CONFIG_SEARCH_SEGMENT)) {
+        auto search_segment =
+            general_section.at(CONFIG_SEARCH_SEGMENT).as_boolean();
+
+        dbg_fmt("Setting {} flag to {}\n", CONFIG_SEARCH_SEGMENT,
+                search_segment);
+        globalConfig.searchSegmentForGadget = search_segment;
+      }
     }
+    // =====================================
 
-    // checking if [functions.default] is in the configuration
-    // the way this check is conducted is silly.
-    if (!configuration_data.contains(CONFIG_FUNCTIONS_SECTION) ||
-        (configuration_data.contains(CONFIG_FUNCTIONS_SECTION) &&
-         (configuration_data.at(CONFIG_FUNCTIONS_SECTION)
-              .count(CONFIG_FUNCTIONS_DEFAULT) == 0))) {
-      fmt::print("Could not find \"[{}]\" section in configuration file.\n",
-                 CONFIG_FUNCTIONS_DEFAULT);
-      exit(-1);
+    /* =====================================
+     * applying [functions.default] section, if present
+     * note: these settings will be overridden if multiple [functions.default]
+     * sections are defined! TODO: fixme
+     */
+    if (configuration_data.contains(CONFIG_FUNCTIONS_SECTION) &&
+        configuration_data.at(CONFIG_FUNCTIONS_SECTION)
+                .count(CONFIG_FUNCTIONS_DEFAULT) != 0) {
+      auto functions_section =
+          toml::find(configuration_data, CONFIG_FUNCTIONS_SECTION);
+      auto default_keys =
+          toml::find(functions_section, CONFIG_FUNCTIONS_DEFAULT);
+
+      // Opaque predicates enabled
+      if (default_keys.contains(CONFIG_OPA_PRED_ENABLED)) {
+        auto op_enabled = default_keys.at(CONFIG_OPA_PRED_ENABLED).as_boolean();
+
+        dbg_fmt("Setting {} flag to {}\n", CONFIG_OPA_PRED_ENABLED, op_enabled);
+        defaultParameter.opaquePredicateEnabled = op_enabled;
+      }
+
+      // Opaque predicates algorithm
+      if (default_keys.contains(CONFIG_OPA_PRED_ALGO)) {
+        auto op_algo = default_keys.at(CONFIG_OPA_PRED_ALGO).as_string();
+
+        dbg_fmt("Setting {} to {} (NOT BEING SET, REALLY)\n",
+                CONFIG_OPA_PRED_ALGO, op_algo);
+
+        // TODO: parse string to extrapolate algorithm
+        // defaultParameter.opaqueConstantAlgorithm = op_algo;
+      }
+
+      // Branch divergence enabled
+      if (default_keys.contains(CONFIG_BRANCH_DIV_ENABLED)) {
+        auto branch_div_enabled =
+            default_keys.at(CONFIG_BRANCH_DIV_ENABLED).as_boolean();
+
+        dbg_fmt("Setting {} flag to {}\n", CONFIG_BRANCH_DIV_ENABLED,
+                branch_div_enabled);
+
+        defaultParameter.opaqueBranchDivergenceEnabled = branch_div_enabled;
+      }
+
+      // Branch divergence max depth
+      if (default_keys.contains(CONFIG_BRANCH_DIV_MAX)) {
+        auto branch_div_max =
+            default_keys.at(CONFIG_BRANCH_DIV_MAX).as_integer();
+
+        dbg_fmt("Setting {} to {}\n", CONFIG_BRANCH_DIV_MAX, branch_div_max);
+
+        defaultParameter.opaqueBranchDivergenceMaxBranches = branch_div_max;
+      }
+
+      // Branch divergence algorithm
+      if (default_keys.contains(CONFIG_BRANCH_DIV_ALGO)) {
+        auto branch_div_algo =
+            default_keys.at(CONFIG_BRANCH_DIV_ALGO).as_string();
+
+        dbg_fmt("Setting {} to {} (NOT BEING SET, REALLY)\n",
+                CONFIG_BRANCH_DIV_ALGO, branch_div_algo);
+
+        // TODO: parse string to extrapolate algorithm
+        // defaultParameter.opaqueBranchDivergenceAlgorithm =
+        // branch_div_algo;
+      }
     }
-
-    toml::value general_section =
-        toml::find(configuration_data, CONFIG_GENERAL_SECTION);
-    toml::value functions_section =
-        toml::find(configuration_data, CONFIG_FUNCTIONS_SECTION);
-
-    dbg_fmt("{}", general_section);
-    dbg_fmt("{}", functions_section);
+    // =====================================
   }
 };
 
