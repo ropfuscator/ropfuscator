0eee0747b978bfdfb5f1bc51cde64e5b1ae924e7
Giulio De Pasquale <giulio@depasquale.eu>
2020-03-11 19:55:31 +0000

    added configuration parsing stub

--- Ropfuscator/ROPfuscatorConfig.h
+++ Ropfuscator/ROPfuscatorConfig.h
@@ -1,11 +1,36 @@
 #ifndef ROPFUSCATORCONFIG_H
 #define ROPFUSCATORCONFIG_H
 
+#include "Debug.h"
+#include "toml.hpp"
 #include <string>
 
 // default configuration (defined in OpaqueConstruct.h)
 extern const std::string OPAQUE_CONSTANT_ALGORITHM_MOV;
 extern const std::string OPAQUE_BRANCH_ALGORITHM_ADDREG_MOV;
+
+/* =========================
+ * CONFIGURATION FILE STRINGS
+ */
+
+#define CONFIG_GENERAL_SECTION "general"
+#define CONFIG_FUNCTIONS_SECTION "functions"
+#define CONFIG_FUNCTIONS_DEFAULT "default"
+
+// general section
+#define CONFIG_OBF_ENABLED "obfuscation_enabled"
+#define CONFIG_SEARCH_SEGMENT "search_segment_for_gadget"
+#define CONFIG_AVOID_MULTIVER "avoid_multiversion_symbol"
+#define CONFIG_CUSTOM_LIB_PATH "custom_library_path"
+
+// functions section
+#define CONFIG_FUNCTION_NAME "name"
+#define CONFIG_OPA_PRED_ENABLED "opaque_predicates_enabled"
+#define CONFIG_OPA_PRED_ALGO "opaque_predicates_algorithm"
+#define CONFIG_BRANCH_DIV_ENABLED "branch_divergence_enabled"
+#define CONFIG_BRANCH_DIV_MAX "branch_divergence_max_branches"
+
+//===========================
 
 /// obfuscation configuration parameter for each function
 struct ObfuscationParameter {
@@ -57,8 +82,49 @@ struct ROPfuscatorConfig {
     // TODO: return per-function configuration (maybe implement in .cpp?)
     return defaultParameter;
   }
+
   void loadFromFile(const std::string &filename) {
-    // TODO: not implemented (maybe implement in .cpp?)
+    dbg_fmt("Loading configuration from file {}.\n", filename);
+
+    toml::value configuration_data;
+
+    try {
+      configuration_data = toml::parse(filename);
+    } catch (const std::runtime_error &e) {
+      // TODO: better output
+      printf("Error while parsing configuration file:\n %s", e.what());
+      exit(-1);
+    } catch (const toml::syntax_error &e) {
+      // TODO: better output
+      printf("Syntax error in configuration file:\n %s", e.what());
+      exit(-1);
+    }
+
+    // checking if [general] is in the configuration
+    if (!configuration_data.contains(CONFIG_GENERAL_SECTION)) {
+      fmt::print("Could not find \"[{}]\" section in configuration file.\n",
+                 CONFIG_GENERAL_SECTION);
+      exit(-1);
+    }
+
+    // checking if [functions.default] is in the configuration
+    // the way this check is conducted is silly.
+    if (!configuration_data.contains(CONFIG_FUNCTIONS_SECTION) ||
+        (configuration_data.contains(CONFIG_FUNCTIONS_SECTION) &&
+         (configuration_data.at(CONFIG_FUNCTIONS_SECTION)
+              .count(CONFIG_FUNCTIONS_DEFAULT) == 0))) {
+      fmt::print("Could not find \"[{}]\" section in configuration file.\n",
+                 CONFIG_FUNCTIONS_DEFAULT);
+      exit(-1);
+    }
+
+    toml::value general_section =
+        toml::find(configuration_data, CONFIG_GENERAL_SECTION);
+    toml::value functions_section =
+        toml::find(configuration_data, CONFIG_FUNCTIONS_SECTION);
+
+    dbg_fmt("{}", general_section);
+    dbg_fmt("{}", functions_section);
   }
 };
 
