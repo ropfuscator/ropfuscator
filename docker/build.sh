8e17d24ebbed399cd02f7d8a91b9507ff694e0e2
Giulio De Pasquale <giulio@depasquale.eu>
2021-05-11 13:06:44 +0100

    bump to llvm-9

--- docker/build.sh
+++ docker/build.sh
@@ -2,6 +2,6 @@
 
 ROPF_DIR=$(dirname $0)/..
 
-docker build -t ropfuscator:prebuild-llvm-7 --target prebuild -f ${ROPF_DIR}/docker/Dockerfile.llvm-7 ${ROPF_DIR}
-docker build -t ropfuscator:build-llvm-7 --target build -f ${ROPF_DIR}/docker/Dockerfile.llvm-7 ${ROPF_DIR}
-docker build -t ropfuscator:llvm-7 --target runtime -f ${ROPF_DIR}/docker/Dockerfile.llvm-7 ${ROPF_DIR}
+docker build -t ropfuscator:prebuild-llvm-9 --target prebuild -f ${ROPF_DIR}/docker/Dockerfile.llvm-9 ${ROPF_DIR}
+docker build -t ropfuscator:build-llvm-9 --target build -f ${ROPF_DIR}/docker/Dockerfile.llvm-9 ${ROPF_DIR}
+docker build -t ropfuscator:llvm-9 --target runtime -f ${ROPF_DIR}/docker/Dockerfile.llvm-9 ${ROPF_DIR}
