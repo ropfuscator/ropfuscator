d08eca452f05371356e2d8155a27a821d0fdb357
Giulio De Pasquale <giulio@depasquale.eu>
2021-05-11 22:47:17 +0100

    debug devenv bootstrap script

---
+++ scripts/setup_devenv.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+ROPFUSCATOR_PATH=`( cd $(dirname $0) && cd .. && pwd )`
+LLVM_VERSION=llvm-10.0.0.src
+CLANG_VERSION=clang-10.0.0.src
+
+LLVM_URL=https://github.com/llvm/llvm-project/releases/download/llvmorg-10.0.0/$LLVM_VERSION.tar.xz
+CLANG_URL=https://github.com/llvm/llvm-project/releases/download/llvmorg-10.0.0/$CLANG_VERSION.tar.xz
+
+# download LLVM
+wget $LLVM_URL
+tar -xf $LLVM_VERSION.tar.xz && rm $LLVM_VERSION.tar.xz
+cd $LLVM_VERSION
+# insert clang inot LLVM source tree
+pushd tools
+
+wget $CLANG_URL
+tar -xf $CLANG_VERSION.tar.xz  && rm $CLANG_VERSION.tar.xz
+
+popd
+# link ropfuscator dir into LLVM source tree
+pushd lib/Target/X86
+
+ln -s $ROPFUSCATOR_PATH .
+patch < ropfuscator/patch/llvm-10.patch
+
+popd
+# create build dir
+mkdir build && cd build
+# config project
+cmake -DBUILD_SHARED_LIBS=OFF -DCMAKE_BUILD_TYPE=Debug -DLLVM_TARGETS_TO_BUILD=X86 -G Ninja ..
+
