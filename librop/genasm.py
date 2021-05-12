ba020e8eac4b29e080440cb34369ef6d82104ad4
Giulio De Pasquale <giulio@depasquale.eu>
2021-05-12 18:50:46 +0100

    generate librop through a c file (CMake workaround)

--- librop/genasm.py
+++ librop/genasm.py
@@ -3,7 +3,7 @@
 from typing import List
 
 
-OUTFILE = "librop.s"
+OUTFILE = "librop.c"
 
 REG2REG_OP = ["xor", "and", "or", "mov", "add", "sub", "xchg", "cmova", "cmovae", "cmovb", "cmovbe", "cmovc", "cmove", "cmovg", "cmovge", "cmovl", "cmovle", "cmovna", "cmovnae",
               "cmovnb", "cmovnbe", "cmovnc", "cmovne", "cmovng", "cmovnge", "cmovnl", "cmovnle", "cmovno", "cmovnp", "cmovns", "cmovnz", "cmovo", "cmovp", "cmovpe", "cmovpo", "cmovs", "cmovz"]
@@ -12,34 +12,30 @@ REGONLY_OP = ["push", "pop"]
 X86_REGS = ["eax", "ebx", "ecx", "edx", "esi", "edi", "esp", "ebp"]
 
 
-def export_directive_template(name):
-    return f".global {name}"
-
-
-def code_template(symname, operation):
-    return f"""{symname}:  {operation}\n\tret"""
-
+def create_function(name, mnemonic, req_op, opt_op = None):
+    return \
+        f"""__attribute__((naked))void {name}(){{
+            __asm {{
+                {mnemonic} {",".join([x for x in [req_op, opt_op] if x])}
+                ret
+            }}
+        }}"""
 
 def main():
-    exports: List[str] = []
     functions: List[str] = []
 
     for op in REG2REG_OP:
         for dst in X86_REGS:
             for src in X86_REGS:
                 symname = f"{op}_{dst}_{src}"
-                exports.append(export_directive_template(symname))
-                functions.append(code_template(symname, f"{op} {dst}, {src}"))
+                functions.append(create_function(symname, op, dst, src))
 
     for op in REGONLY_OP:
         for r in X86_REGS:
             symname = f"{op}_{r}"
-            exports.append(export_directive_template(symname))
-            functions.append(code_template(symname, f"{op} {r}"))
+            functions.append(create_function(symname, op, r))
 
     with open(OUTFILE, "w") as f:
-        f.write("\n".join(exports))
-        f.write("\n\n.section .text\n\n")
         f.write("\n".join(functions))
 
     return
