d3bc50d1aa443945288feda0d5779df61e2ce9ac
Giulio De Pasquale <giulio@depasquale.eu>
2021-05-26 02:19:54 +0100

    angr solve time benchmark script

---
+++ evaluation/tigress/scripts/benchmark_solve_time.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SCRIPT_PATH=$( (cd $(dirname $0) && pwd))
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <directory_with_tigress_samples>"
+    exit
+fi
+
+for vanilla_sample in $(find $1 -maxdepth 1 -type f -name "vanilla-tigsam*"); do
+    echo "Evaluating $vanilla_sample..."
+    \time -f "%C, %e" -a -o angr_solve_times.csv python $SCRIPT_PATH/solve_tigress_chall.py $vanilla_sample
+done
