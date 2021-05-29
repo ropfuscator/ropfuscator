8be1fcb155403303b1dd4c5de819b1dcb767da97
Giulio De Pasquale <giulio@depasquale.eu>
2021-05-30 00:09:19 +0100

    build.sh points to dockerfile in ropfuscator directory

--- docker/build.sh
+++ docker/build.sh
@@ -2,6 +2,6 @@
 
 ROPF_DIR=$(dirname $0)/..
 
-docker build -t ropfuscator:prebuild --target prebuild -f ${ROPF_DIR}/docker/Dockerfile ${ROPF_DIR}
-docker build -t ropfuscator:build --target build -f ${ROPF_DIR}/docker/Dockerfile ${ROPF_DIR}
-docker build -t ropfuscator:release --target runtime -f ${ROPF_DIR}/docker/Dockerfile
+docker build -t ropfuscator:prebuild --target prebuild -f ${ROPF_DIR}/docker/ropfuscator/Dockerfile ${ROPF_DIR}
+docker build -t ropfuscator:build --target build -f ${ROPF_DIR}/docker/ropfuscator/Dockerfile ${ROPF_DIR}
+docker build -t ropfuscator:release --target runtime -f ${ROPF_DIR}/docker/ropfuscator/Dockerfile
