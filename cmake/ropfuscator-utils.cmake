65288d3c2e2ee5a7da992be5c8a3a78792c4ba65
Giulio De Pasquale <giulio@depasquale.eu>
2021-05-31 23:59:15 +0100

    aggregated flags in intermediate variables. generate_clean_asm now calls generate_ropfuscated_asm to reduce code duplication

--- cmake/ropfuscator-utils.cmake
+++ cmake/ropfuscator-utils.cmake
@@ -1,57 +1,69 @@
 macro(generate_ropfuscated_asm)
-  set(oneValueArgs SOURCE OUTNAME)
+  #
+  # macro argument parsing
+  #
+
+  set(oneValueArgs SOURCE OUTNAME OBF_CONFIG)
   set(multiValueArgs IRFLAGS ASMFLAGS)
 
   cmake_parse_arguments(ARG "${options}" "${oneValueArgs}" "${multiValueArgs}"
                         ${ARGN})
 
-  if(ROPF_PROFILE)
-    set(ROPF_IR_FLAGS ${ROPF_IR_FLAGS} ${COMPILER_PROFILING_FLAGS}
-                      -fprofile-instr-generate=${ARG_OUTNAME}.profdata)
-  endif()
+  #
+  # getting includes directories set for current directory
+  #
 
-  get_filename_component(filename ${ARG_SOURCE} NAME_WE)
-
-  # constructing the includes directives
   get_directory_property(CURRENT_DIR_INCLUDES DIRECTORY . INCLUDE_DIRECTORIES)
+
   foreach(dir ${CURRENT_DIR_INCLUDES})
     string(APPEND INCLUDES_DIRECTIVE "-I${dir} ")
   endforeach()
 
+  #
+  # macro variables
+  #
+
+  set(CLANG_FLAGS ${ROPF_IR_FLAGS} ${INCLUDES_DIRECTIVE} ${ARG_IRFLAGS}
+                  ${ARG_SOURCE})
+  set(LLC_FLAGS ${ROPF_ASM_FLAGS} ${ARG_ASMFLAGS} ${ARG_OUTNAME}.bc)
+
+  #
+  # options handling
+  #
+
+  if(ROPF_PROFILE)
+    list(APPEND CLANG_FLAGS ${COMPILER_PROFILING_FLAGS}
+         -fprofile-instr-generate=${ARG_OUTNAME}.profdata)
+  endif()
+
   add_custom_command(
     OUTPUT ${ARG_OUTNAME}.s
     DEPENDS ${ARG_SOURCE}
-    COMMAND $<TARGET_FILE:clang> ARGS ${INCLUDES_DIRECTIVE} ${ROPF_IR_FLAGS}
-            ${ARG_IRFLAGS} ${ARG_SOURCE} -o ${ARG_OUTNAME}.bc
-    COMMAND $<TARGET_FILE:llc> ARGS ${ROPF_ASM_FLAGS} ${ARG_ASMFLAGS}
-            ${ARG_OUTNAME}.bc -o ${ARG_OUTNAME}.s)
+    COMMAND $<TARGET_FILE:clang> ARGS ${CLANG_FLAGS} -o ${ARG_OUTNAME}.bc
+    COMMAND $<TARGET_FILE:llc> ARGS ${LLC_FLAGS} -o ${ARG_OUTNAME}.s)
 endmacro()
 
 macro(generate_clean_asm)
+  #
+  # macro argument parsing
+  #
+
   set(oneValueArgs SOURCE OUTNAME)
   set(multiValueArgs IRFLAGS ASMFLAGS)
 
   cmake_parse_arguments(ARG "${options}" "${oneValueArgs}" "${multiValueArgs}"
                         ${ARGN})
 
-  if(ROPF_PROFILE)
-    set(ROPF_IR_FLAGS ${ROPF_IR_FLAGS} ${COMPILER_PROFILING_FLAGS}
-                      -fprofile-instr-generate=${ARG_OUTNAME}.profdata)
-  endif()
+  # add no-ropfuscator flag to user defined flags
+  list(APPEND ARG_ASMFLAGS -fno-ropfuscator)
 
-  get_filename_component(filename ${ARG_SOURCE} NAME_WE)
-
-  # constructing the includes directives
-  get_directory_property(CURRENT_DIR_INCLUDES DIRECTORY . INCLUDE_DIRECTORIES)
-  foreach(dir ${CURRENT_DIR_INCLUDES})
-    string(APPEND INCLUDES_DIRECTIVE "-I${dir} ")
-  endforeach()
-
-  add_custom_command(
-    OUTPUT ${ARG_OUTNAME}.s
-    DEPENDS ${ARG_SOURCE}
-    COMMAND $<TARGET_FILE:clang> ARGS ${INCLUDES_DIRECTIVE} ${ROPF_IR_FLAGS}
-            ${ARG_IRFLAGS} ${ARG_SOURCE} -o ${ARG_OUTNAME}.bc
-    COMMAND $<TARGET_FILE:llc> ARGS -fno-ropfuscator ${ROPF_ASM_FLAGS}
-            ${ARG_ASMFLAGS} ${ARG_OUTNAME}.bc -o ${ARG_OUTNAME}.s)
+  generate_ropfuscated_asm(
+    SOURCE
+    ${ARG_SOURCE}
+    OUTNAME
+    ${ARG_OUTNAME}
+    ASMFLAGS
+    ${ARG_ASMFLAGS}
+    IRFLAGS
+    ${ARG_IRFLAGS})
 endmacro()
