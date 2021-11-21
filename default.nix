c9bd18e50275d22953178c5dbcd540444d964ce8
Giulio De Pasquale <depasquale+git@giugl.io>
2021-11-21 11:56:56 +0100

    use fetchGit instead of local sources

--- default.nix
+++ default.nix
@@ -21,13 +21,22 @@ let
   python-deps = python-packages: with python-packages; [ pygments ];
   python = pkgs.python3.withPackages python-deps;
 
+  ropfuscator_repo = builtins.fetchGit {
+    url = "git@github.com:ropfuscator/ropfuscator.git";
+    submodules = true;
+  };
   derivation_function = { stdenv, cmake, ninja, z3, git, SDL2, SDL2_net
     , SDL2_mixer, pkg-config, libxml2, curl, openal, libpng, libsamplerate }:
     stdenv.mkDerivation {
       pname = "ropfuscator";
       version = "0.1.0";
       nativeBuildInputs = [ cmake ninja git curl python pkg-config z3 ];
-      srcs = [ ./cmake ./src ./CMakeLists.txt ./thirdparty ];
+      srcs = [
+        "${ropfuscator_repo}/cmake"
+        "${ropfuscator_repo}/src"
+        "${ropfuscator_repo}/CMakeLists.txt"
+        "${ropfuscator_repo}/thirdparty"
+      ];
       patches = [ ./patches/ropfuscator_pass.patch ];
       postPatch = "patchShebangs .";
 
