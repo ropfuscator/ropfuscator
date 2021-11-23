e9f77c0f2d264ad47bcc65e5d86bdc6b38498d11
Giulio De Pasquale <peperunas@users.noreply.github.com>
2021-11-23 18:02:47 +0000

    Delete flake.nix

--- flake.nix
+++
@@ -1,8 +0,0 @@
-{
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (
-      let pkgs = nixpkgs.legacyPackages.i686-linux;
-      in { defaultPackage = pkgs.callPackage ./default.nix { }; });
-}
