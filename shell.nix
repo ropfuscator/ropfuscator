5c149555b607bc71780a73a3b99d3a25c08231a3
Giulio De Pasquale <depasquale+git@giugl.io>
2021-11-23 13:41:28 +0100

    added librop to env in shell

--- shell.nix
+++ shell.nix
@@ -8,10 +8,14 @@ let
   }) { });
   pkgs = pkgs64.pkgsi686Linux;
   ropf_mod = import ./default.nix { };
+  librop = import "${
+      builtins.fetchGit { url = "git@github.com:ropfuscator/librop.git"; }
+    }/default.nix" { };
   myStdenv = pkgs.mkShell.override { stdenv = ropf_mod.stdenv; };
 in myStdenv {
-  nativeBuildInputs = ropf_mod.ropfuscator.nativeBuildInputs;
+  nativeBuildInputs = ropf_mod.ropfuscator.nativeBuildInputs ++ [ librop ];
   shellHook = ''
+    export LIBROP=${librop}/lib/librop.so
     export ROPFUSCATOR_LLC=${ropf_mod.ropfuscator}/bin/llc
     export ROPFUSCATOR_CLANG=${ropf_mod.stdenv.cc}/bin/clang
   '';
