32fcfad4bf8f914c2b3aa30d9af42c6bd9712707
Giulio De Pasquale <depasquale+git@giugl.io>
2022-08-25 18:25:10 +0200

    Revert "Remove old shellHook"
    This reverts commit 222da237bb35b28a4e65f321dac29aacae7840ec.

--- flake.nix
+++ flake.nix
@@ -195,6 +195,17 @@
           inherit pkgs;
           stdenv = packages.vanillaRopStdenv;
         };
+        # pkgs.buildPackages.ropfuscator-llvm-debug.overrideAttrs (_: {
+        #   shellHook = ''
+        #     # move to temporary directory
+        #     cd `mktemp -d`
+        #     # unpack and configure project
+        #     echo "Preparing LLVM source tree..."
+        #     eval "$unpackPhase" && cd llvm && runHook patchPhase && eval "$configurePhase"
+        #     # get compile_commands.json and put them in root of LLVM tree
+        #     cd .. && mv build/compile_commands.json .
+        #   '';
+        # });
 
         # exposed packages
         packages = flake-utils.lib.flattenTree rec {
